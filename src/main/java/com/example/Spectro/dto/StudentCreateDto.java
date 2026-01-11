abe29e2b0df8a0a84eb0fa25618e212a71a01ee2
omarsalem22 <omarmohamed25112001@gmail.com>
2026-01-11 13:57:51 +0200

    ​Implement global exception

--- src/main/java/com/example/Spectro/dto/StudentCreateDto.java
+++ src/main/java/com/example/Spectro/dto/StudentCreateDto.java
@@ -1,7 +1,10 @@
 package com.example.Spectro.dto;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,10 +17,16 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @Builder
 public class StudentCreateDto {
+
+    @Size(min = 2, max = 50, message = "firstName should be more than 2 chars")
     private String firstName;
+
+    @Size(min = 2, max = 50, message = "lastName should be more than 2 chars")
 
     private String lastName;
 
+    @NotBlank(message = "Email is required")
+    @Email  (message = "Please provide a valid email address")
     private String email;
 
     @Past(message = "Date of birth must be in the past")  // to  ensure the stdent was born in past date
---
+++ src/main/java/com/example/Spectro/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.example.Spectro.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidation(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors()
+                .forEach(err -> errors.put(err.getField(), err.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+    }
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntime(RuntimeException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+}
